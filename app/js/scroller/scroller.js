8ea06f4f7342873fb1563660957e94042518e4e8
John Dyer <johndyer@gmail.com>
2013-10-13 22:05:57 -0500

    event listener

--- app/js/scroller/scroller.js
+++ app/js/scroller/scroller.js
@@ -1,36 +1,3 @@
-var EventEmitter = {
-	events: [],
-
-	addEventListener: function (eventName, callback, bubble) {
-		this.events[eventName] = this.events[eventName] || [];
-		this.events[eventName].push(callback);
-	},
-	removeEventListener: function (eventName, callback) {
-		if (!eventName) { this.events = {}; return true; }
-		var callbacks = this.events[eventName];
-		if (!callbacks) return true;
-		if (!callback) { this.events[eventName] = []; return true; }
-		for (i = 0; i < callbacks.length; i++) {
-			if (callbacks[i] === callback) {
-				this.events[eventName].splice(i, 1);
-				return true;
-			}
-		}
-		return false;
-	},	
-	dispatchEvent: function (eventName) {
-		var i,
-			args,
-			callbacks = this.events[eventName];
-
-		if (callbacks) {
-			args = Array.prototype.slice.call(arguments, 1);
-			for (i = 0; i < callbacks.length; i++) {
-				callbacks[i].apply(null, args);
-			}
-		}
-	}
-};
 
 var Scroller = function(node) {
 	

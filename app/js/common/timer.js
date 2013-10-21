ab0deb3f119cef6cd5ccda2aeb0463702fa11bcb
John Dyer <johndyer@gmail.com>
2013-10-20 23:01:41 -0500

    simple timer class

---
+++ app/js/common/timer.js
@@ -0,0 +1,21 @@
+	
+	var Timer = function(callback, seconds) {
+		
+		var timeoutValue = null;
+		function start() {
+			clear();
+			
+			timeoutValue = setTimeout(callback, seconds);
+		}
+		function clear() {
+			if (timeoutValue != null) {
+				clearTimeout(timeoutValue);
+				timeoutValue = null;
+			}
+		}
+		
+		return {
+			start: start,
+			clear: clear
+		}	
+	}
--- app/js/windows/windowapp.js
+++ app/js/windows/windowapp.js
@@ -53,27 +53,6 @@ var App = function() {
 		windowManager.add(windowSetting.type, windowSetting.data);	
 	}
 	
-	var Timer = function(callback, seconds) {
-		
-		var timeoutValue = null;
-		function start() {
-			clear();
-			
-			timeoutValue = setTimeout(callback, seconds);
-		}
-		function clear() {
-			if (timeoutValue != null) {
-				clearTimeout(timeoutValue);
-				timeoutValue = null;
-			}
-		}
-		
-		return {
-			start: start,
-			clear: clear
-		}	
-	}
-	
 	function storeSettings() {
 			// get settings from al windows
 		var windowSettings = windowManager.getSettings(),		

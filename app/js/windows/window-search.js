718a73a4d95f1368c2cc54b3466a2c40b838517c
johndyer <johndyer@gmail.com>
2013-11-07 07:02:58 -0600

    added footer with output messaging

--- app/js/windows/window-search.js
+++ app/js/windows/window-search.js
@@ -7,16 +7,21 @@ var SearchWindow = function(id, parentNode, init_data) {
 						
 						'<input type="text" class="search-text header-input" placeholder="Search" />' + 
 						'<input type="button" value="Search" class="search-button header-button" />' + 		
-						'<select class="search-list header-list" style="max-width: 150px; float: right;" ></select>' + 				
+						'<div class="text-list header-list" style="">&nbsp;</div>' + 
+						//'<select class="search-list header-list" style="max-width: 100px; top: 22px; right: 5px; position: absolute;" ></select>' + 				
 						
 					'</div>').appendTo(parentNode),
-		main = $('<div class="search-main" style="overflow: auto;"><div style="padding: 10px;" class="search-wrapper"></div></div>').appendTo(parentNode),
+		main = $('<div class="search-main" style="background: #fff; overflow: auto;"><div style="padding: 10px;" class="search-wrapper"></div></div>').appendTo(parentNode),
+		footer = $('<div class="search-footer window-footer"></div>').appendTo(parentNode),
 		wrapper = main.find('.search-wrapper'),
 		input = header.find('.search-text'),
 		button = header.find('.search-button'),		
-		list = header.find('.search-list'),
+		//list = header.find('.search-list'),
+		textui = header.find('.text-list'),
+		textChooser = new TextChooser(parentNode),
 		//encoder = new base32.Encoder(),
-		textSearch = new texts.TextSearch();			
+		textSearch = new texts.TextSearch(),
+		selectedText = null;			
 	
 	// EVENTS
 	input.on('keypress', function(e) {
@@ -26,9 +31,18 @@ var SearchWindow = function(id, parentNode, init_data) {
 	});				
 	button.on('click', doSearch);
 	
+	textChooser.on('change', function(e) {
+		selectedText = e.data;
+		textui.html( e.data.abbr );
+	});
+	textui.on('click', function(e) {
+		textChooser.show();
+	});	
+	
+	
 	textSearch.on('load', function(e) {
 		
-		console.log('searcher...');
+		footer.html('Loading... ' + e.data.sectionid);
 		
 	});
 	
@@ -62,6 +76,8 @@ var SearchWindow = function(id, parentNode, init_data) {
 		var results = e.data.results,
 			html = '<h2>Results: ' + results.length + '</h2>' + 
 					'<table cellpadding="2">';
+					
+		footer.html('Results: ' + results.length );
 		
 		for (var i=0, il=results.length; i<il; i++) {
 			var result = results[i],
@@ -90,7 +106,9 @@ var SearchWindow = function(id, parentNode, init_data) {
 		disable();	
 
 		var text = input.val(),
-			textid = list.val();
+			//textid = list.val(),
+			textid = textChooser.getTextInfo().id;
+			
 			
 		console.log('search', textid, text);
 		
@@ -112,13 +130,15 @@ var SearchWindow = function(id, parentNode, init_data) {
 	function size(width, height) {
 		
 		header.outerWidth(width);
+		footer.outerWidth(width);
 		
 		main.outerWidth(width)
-			.outerHeight(height - header.outerHeight());
+			.outerHeight(height - header.outerHeight() - footer.outerHeight());
 	}
 
 	// init
 	function init() {
+		/*
 		texts.Texts.loadTexts(function(data) {
 			
 			var html = '';
@@ -134,13 +154,44 @@ var SearchWindow = function(id, parentNode, init_data) {
 			} else {		
 				list.children().first().attr('selected', true);
 			}
-			
+
 			if (init_data.searchString && init_data.searchString != '') {
 				input.val(init_data.searchString);
 				doSearch();
-			}
+			}				
 			
-		});			
+		});	
+		
+		*/	
+		
+		
+	
+		if (init_data.textid) {
+			texts.Texts.getText(init_data.textid, function(data) {
+					
+				selectedText = data;
+				
+				console.log('search', init_data.textid, data);	
+					
+				textChooser.setTextInfo(selectedText);
+				textui.html(selectedText.abbr);
+	
+				if (init_data.searchString && init_data.searchString != '') {
+					input.val(init_data.searchString);
+					doSearch();
+				}	
+				
+			});						
+		} else {	
+			console.log('SEARCH: no init textid');
+			
+			for (var index in texts.Texts.textData) {
+				var textInfo = texts.Texts.textData[index];
+				textChooser.setTextInfo(textInfo);
+				textui.html(textInfo.abbr);
+				break;
+			}
+		}	
 	}
 	init();
 
@@ -150,7 +201,7 @@ var SearchWindow = function(id, parentNode, init_data) {
 		
 			return {
 				searchString: input.val(),
-				textid: list.val()
+				textid: (selectedText != null) ? selectedText.id : null
 			}			
 		}
 	};	

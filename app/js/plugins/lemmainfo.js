b4d7d93f6c9d4e612f245ff94bc4d81d8564bf11
johndyer <johndyer@gmail.com>
2013-11-08 10:57:09 -0600

    updated box style

--- app/js/plugins/lemmainfo.js
+++ app/js/plugins/lemmainfo.js
@@ -2,20 +2,23 @@
 var LemmaInfoPlugin = function(app) {
 
 	$(
-'<style>' + 
-'.lemma-info {'+
-'position: absolute;'+
-'padding: 5px;'+
-'background: #f8f8f8;'+
-'z-index: 100;'+
-'top: 100px;'+
-'left: 100px;'+
-'font-size: 12px;'+
-'color: #111;'+
-'}' +
-'</style>').appendTo( $('head') );
+'<style>\
+.lemma-info {\
+position: absolute;\
+padding: 5px;\
+background: #f8f8f8;\
+z-index: 100;\
+top: 100px;\
+left: 100px;\
+font-size: 12px;\
+color: #111;\
+font-family: Avenir, Helvetica;\
+-xbox-shadow: 0 0 5px rgba(50,50,50,0.5);\
+border-bottom: solid 1px #ccc;\
+}\
+</style>').appendTo( $('head') );
 
-	var lemmaInfo = $('<div class="lemma-info"></div>').appendTo( $(document.body) );
+	var lemmaInfo = $('<div class="lemma-info"></div>').appendTo( $(document.body) ).hide();
 
 	$('.windows-main').on('mouseover','l', function(e) {
 		
@@ -23,18 +26,20 @@ var LemmaInfoPlugin = function(app) {
 			morph = l.attr('m'),
 			strongs = l.attr('s'),
 			main = l.closest('.scroller-main'),
-			mainOffset = main.offset();
+			mainOffset = main.offset(),
+			morphInfo = bible.morphology.Greek.getMorphology(morph);
 			
 		
-		
-		lemmaInfo.html( bible.morphology.Greek.getMorphology(morph)  );
-		
-		lemmaInfo.show();
-		
-		lemmaInfo.css({
-					left: mainOffset.left + 15 + 'px',
-					top: mainOffset.top + main.outerHeight() - 30 + 'px'
-					});
+		if (morphInfo != null && morphInfo != '') {
+			lemmaInfo.html( morphInfo );
+			
+			lemmaInfo.show();
+			
+			lemmaInfo.css({
+						left: mainOffset.left + 15 + 'px',
+						top: mainOffset.top + main.outerHeight() - 30 + 'px'
+						});
+		}
 		
 		
 	}).on('mouseout','l', function(e) {

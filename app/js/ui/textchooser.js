b4000218f9115100fa295d3af9d9ce8a32b13eaa
johndyer <johndyer@gmail.com>
2013-11-10 22:17:52 -0600

    headers and filter

--- app/js/ui/textchooser.js
+++ app/js/ui/textchooser.js
@@ -4,19 +4,26 @@
 TextChooser
 *******************/
 
-var TextChooser = function(container) {
+var TextChooser = function(container, target) {
 	// create me
-	var selectedTextInfo = null,
+	var 
+		isFull = false,
+		showHeaders = true,
+		selectedTextInfo = null,
 		textSelector = $('<div class="text-chooser">' + 
+							'<span class="up-arrow"></span>' +
+							'<span class="up-arrow-border"></span>' +
 							'<div class="text-chooser-header">' +
-								'<span class="text-chooser-title"></span>' +  
-								'<span class="text-chooser-close">Close</span>' +  
+								'<input type="text" class="text-chooser-filter-text" />' +
+								'<span class="text-chooser-close">Close</span>' +  						
 							'</div>' +
 							'<div class="text-chooser-main"></div>' +		
 						'</div>')
 						.appendTo( $('body') )
 						.hide(),
+		header = textSelector.find('.text-chooser-header'),
 		main = textSelector.find('.text-chooser-main'),
+		filter = textSelector.find('.text-chooser-filter-text'),
 		title = textSelector.find('.text-chooser-title'),
 		close = textSelector.find('.text-chooser-close');
 		
@@ -24,6 +31,62 @@ var TextChooser = function(container) {
 	title.html("Texts");
 	
 	close.on('click', hide);
+	
+	
+	filter.on('keyup keypress', filterVersions);
+	
+	function filterVersions(e) {
+	
+		if (e.which == 13) {
+			var visibleRows = main.find('.text-chooser-row:visible');
+			
+			if (visibleRows.length == 1) {
+				
+				visibleRows.click();
+				
+				filter.val('');
+				return;
+			}
+		}
+	
+			
+		var text = filter.val();
+		
+		if (text == '') {
+			main.find('.text-chooser-row-header, .text-chooser-row')
+					.removeClass('filtered')
+					.show();			
+		} else {
+			
+			text = text.toLowerCase();
+		
+			// hide the headers
+			main.find('.text-chooser-row-header').hide();
+			
+			main.find('.text-chooser-row').each(function() {
+				var row = $(this),
+					abbr = row.find('.text-chooser-abbr'),
+					name = row.find('.text-chooser-name');
+					
+				if (
+					row.attr('data-native-name').toLowerCase().indexOf(text) > -1 ||
+					row.attr('data-english-name').toLowerCase().indexOf(text) > -1 ||					
+					name.text().toLowerCase().indexOf(text) > -1 ||					
+					abbr.text().toLowerCase().indexOf(text) > -1) {
+						
+					row.show().addClass('filtered');
+						
+				} else {
+				
+					row.hide().removeClass('filtered');
+					
+				}
+				
+			});
+			
+		}
+		
+	}
 	
 	// handle when user clicks on a text
 	textSelector.on('click', '.text-chooser-row', function() {
@@ -39,7 +102,7 @@ var TextChooser = function(container) {
 		
 		hide();
 		
-		console.log('chooser:change:click', selectedTextInfo);
+		//console.log('chooser:change:click', selectedTextInfo);
 		ext.trigger('change', {type:'change', target: this, data: selectedTextInfo});
 	});
 	
@@ -47,18 +110,92 @@ var TextChooser = function(container) {
 	function renderTexts() {
 	
 		// render all the rows
-		var html = [];
-	
-		for (var index in texts.Texts.textData) {
-							
-			var text = texts.Texts.textData[index];
+		var html = [],
+			arrayOfTexts = [];
+		
+		// turn object into array
+		for (var key in texts.Texts.textData) {
+			arrayOfTexts.push(texts.Texts.textData[key]);	
+		
+		}
+			
+		
+		if (showHeaders) {
+			// find languages
+			var languages = [];
+			for (var index in arrayOfTexts) {
+				var text = arrayOfTexts[index];
+				
+				if (languages.indexOf(text.lang) == -1) {
+					languages.push( text.lang );					
+				}				
+			}
+			
+			// remove English
+			var englishIndex = languages.indexOf('eng');
+			if (englishIndex > -1) {
+				languages.splice(englishIndex, 1);				
+			}
+			languages.sort();
+			if (englishIndex > -1) {
+				languages.splice(0,0,'eng');
+			}
+			
+			
+			
+			for (var index in languages) {
+			
+				// get all the ones with this langu
+				var lang = languages[index],
+					textsInLang = arrayOfTexts.filter(function(t) { if (t.lang == lang) { return t; } });
+				
 					
-			html.push('<div class="text-chooser-row" data-id="' + text.id + '">' +
-							'<span class="text-chooser-abbr">' + text.abbr + '</span>' +
-							'<span class="text-chooser-name">' + text.name + '</span>' +
-						'</div>'
-			);
-		}		
+				html.push('<div class="text-chooser-row-header">' +
+						textsInLang[0].nativeName + ' ('  + textsInLang[0].englishName + ')' +
+							'</div>'
+				);				
+				
+				for (var textIndex in textsInLang) {
+					var text = textsInLang[textIndex];
+					
+					
+					html.push('<div class="text-chooser-row" data-id="' + text.id + '" data-native-name="' + text.nativeName + '" data-english-name="' + text.englishName + '">' +
+									'<span class="text-chooser-abbr">' + text.abbr + '</span>' +
+									'<span class="text-chooser-name">' + text.name + '</span>' +
+								'</div>'
+					);					
+				}
+				
+			}
+			
+			
+			
+		} else {
+			
+			// sort by name
+			arrayOfTexts.sort(function(a, b) {
+				
+				if (a.abbr > b.abbr)
+					return 1;
+				if (a.abbr < b.abbr)
+					return -1;
+				// a must be equal to b
+				return 0;
+					
+			});
+					
+			for (var index in arrayOfTexts) {
+								
+				var text = arrayOfTexts[index];
+						
+				html.push('<div class="text-chooser-row" data-id="' + text.id + '">' +
+								'<span class="text-chooser-abbr">' + text.abbr + '</span>' +
+								'<span class="text-chooser-name">' + text.name + '</span>' +
+							'</div>'
+				);
+			}			
+		}
+	
 		main.html(html);
 		
 		
@@ -73,8 +210,8 @@ var TextChooser = function(container) {
 	}
 	
 	function show() {
-		size();
 		
+		size();
 	
 		if (texts.Texts.finishedLoading) {
 			renderTexts();
@@ -88,6 +225,9 @@ var TextChooser = function(container) {
 		}
 	
 		textSelector.show();
+		size();
+		filter.val('').focus();
+
 	}
 	
 	function hide() {
@@ -105,20 +245,46 @@ var TextChooser = function(container) {
 	
 	function size(width,height) {
 	
-		if (!(width && height)) {
-			width = container.width();
-			height = container.height();			
+		if (isFull) {
+			
+			// cover the container area
+			if (!(width && height)) {
+				width = container.width();
+				height = container.height();			
+			}
+		
+			textSelector
+				.width(width)
+				.height(height)
+				.css({top: container.offset().top,left: container.offset().left});
+				
+			main
+				.width(width)
+				.height(height - header.outerHeight())
+				
+		} else {
+			// reasonable size!
+			var top = target.offset().top + target.outerHeight() + 5,
+				left = target.offset().left,
+				winHeight = $(window).height() - 40,
+				maxHeight = winHeight - top;
+			
+			textSelector
+				.outerHeight(maxHeight)
+				.css({top: top,left: left});
+				
+			console.log('CHOOSER size', maxHeight, header.outerHeight(), maxHeight -  header.outerHeight());
+				
+			main
+				.outerHeight(maxHeight - header.outerHeight())				
+			
 		}
-	
-		textSelector
-			.width(width)
-			.height(height)
-			.css({top: container.offset().top,left: container.offset().left});
 	}	
 	
 	var ext = {
 		show: show,
 		hide: hide,
+		toggle: toggle,
 		getTextInfo: getTextInfo,
 		setTextInfo: setTextInfo,
 		renderTexts: renderTexts,

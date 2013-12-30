8d795ab1f7fdf06a42a3a70e35c41d2b1d0d2df4
John Dyer <johndyer@gmail.com>
2013-12-30 08:00:56 -0600

    Creating a build process

---
+++ app/js/common/stringutility.js
@@ -0,0 +1,19 @@
+var stringUtility = {
+	
+	// get from URL
+	parseQuerystring: function (querystring) {
+		
+		querystring = querystring || window.location.search.substring(1);
+	
+		// remove any preceding url and split
+		querystring = querystring.substring(querystring.indexOf('?')+1).split('&');
+		var params = {}, pair, d = decodeURIComponent;
+		// march and parse
+		for (var i = querystring.length - 1; i >= 0; i--) {
+		pair = querystring[i].split('=');
+		params[d(pair[0])] = d(pair[1]);
+		}
+		
+		return params;
+	}
+}

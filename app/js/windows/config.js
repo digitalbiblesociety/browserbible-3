da38fb18d45944b2ba61e67dd18eac7773650b51
John Dyer <johndyer@gmail.com>
2013-12-31 07:12:07 -0600

    updates to first load variables

--- app/js/windows/config.js
+++ app/js/windows/config.js
@@ -2,12 +2,16 @@ sofia.config = {
 	// Enables the use of online sources (Google Maps, FCBH, Jesus Film, etc.)
 	enableOnlineSources: true,
 	
-	// defaults for new text window
+	// first load
 	windows: [
-		{type: 'bible', data: {textid: 'eng_nasb', fragmentid: 'JN1_1'}},
-		{type: 'bible', data: {textid: 'grc_tisch', fragmentid: 'JN1_1'}},
-		{type: 'search', data: {textid: 'eng_nasb', searchtext:'truth love'}}		
+		{type: 'bible', data: {textid: 'eng-NASB1995', fragmentid: 'JN1_1'}},
+		{type: 'bible', data: {textid: 'grc-tisch', fragmentid: 'JN1_1'}},
+		{type: 'search', data: {textid: 'eng-NASB1995', searchtext:'truth love'}}		
 	],
+	
+	// new window
+	newBibleWindowVersion: 'eng-NASB1995',
+	newBibleWindowVerse: 'JN1_1',
 	
 	// Faith Comes by Hearing
 	fcbhKey: '',

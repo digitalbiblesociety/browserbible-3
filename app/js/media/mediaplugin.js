1e2d1744d82190d716f13a2ad4fa83d646e00036
John Dyer <johndyer@gmail.com>
2014-01-26 23:37:46 -0600

    rearranged to make room for more types.

--- app/js/media/mediaplugin.js
+++ app/js/media/mediaplugin.js
@@ -5,10 +5,90 @@ var MediaLibraryPlugin = function(app) {
 		mediaPopup = new InfoWindow(),
 		contentToProcess = [];
 	
+	
+		console.log('MediaLibraryPlugin startup');
+	
 	MediaLibrary.getMediaLibraries(function(data) {
 		mediaLibraries = data;
+		
+		setupMediaEvents();
+		
 		addMedia();		
 	});
+	
+	function setupMediaEvents() {
+	
+		// handle clicks
+		$('.windows-main').on('click', '.mediathumb', function(e) {
+		
+			console.log('media click', mediaLibrary);
+	
+			// clear it out!
+			//mediaPopup.body.html('');				
+	
+			var 
+				icon = $(this),
+				mediaFolder = icon.attr('data-mediafolder'),
+				verse = icon.closest('.verse, .v'),
+				verseid = verse.attr('data-id'),
+				reference = new bible.Reference(verseid).toString(),
+				mediaLibrary = null,
+				mediaForVerse = null;
+				
+			// find library
+			for (var i=0, il=mediaLibraries.length; i<il; i++ ) {
+				var ml = mediaLibraries[i]
+				
+				if (ml.folder == mediaFolder) {
+					mediaLibrary = ml;
+					break;
+				}
+				
+			}	
+				
+			mediaForVerse = mediaLibrary.data[verseid];
+				
+			switch (mediaLibrary.type) {
+				
+				case 'image':
+												
+					var imagesHtmlArray = $.map(mediaForVerse, function(item, index) {
+						var url = 'content/media/' + mediaLibrary.folder + '/' + item;
+							return '<li><a href="' + url + '" target="_blank"><img src="' + url + '" /></a></li>';
+						}),					
+						html = imagesHtmlArray.join('');
+
+					mediaPopup.body.append('<strong>' + reference.toString() + '</strong>');	
+					mediaPopup.body.append($('<ul class="inline-image-library-thumbs">' + html + '</ul>'));
+					
+					break;
+				
+				case 'video':
+					var url = 'content/media/' + mediaLibrary.folder + '/' + mediaForVerse;																
+					//mediaPopup.body.append('<video src="' + url + '" type="video/mp4" controls autoplay style="width:100%; height: auto;"></video>');
+					
+					mediaPopup.body.append('<strong>' + reference.toString() + '</strong>');	
+					mediaPopup.body.append($('<ul class="inline-video-library-thumbs">' + 
+						'<li><a href="' + url + '"><img src="css/images/video.svg" "></a></li>' +
+					  '</ul>'));
+
+					
+					break;	
+					
+				case 'jfm':
+
+					// console.log
+					mediaPopup.body.append('<div>Jesus Film!</div>');
+					
+					break;																					
+					
+			}
+
+			//mediaPopup.center().show();
+			mediaPopup.position( icon ).show();
+		});
+		
+	}
 	
 	// process chapters, add image icon to verses
 	function addMedia() {
@@ -31,6 +111,10 @@ var MediaLibraryPlugin = function(app) {
 				
 				// make sure we're just doing the first verse	
 				verse = verse.parent().find('.' + verseid).first();
+				
+				if (verseid == 'LK1_1') {
+					console.log('check');
+				}
 					
 				if (!verse.hasClass('has-media')) {
 					// check all libraries
@@ -43,9 +127,9 @@ var MediaLibraryPlugin = function(app) {
 						if (typeof mediaForVerse != 'undefined') {	
 							
 							// check if it's alrady been added
-							if (verse.closest('.chapter').find('.' + verseid).find('.' + iconClassName).length == 0) {
+							//if (verse.closest('.chapter').find('.' + verseid).find('.' + iconClassName).length == 0) {
 						
-								var icon = $('<span class="inline-icon ' + iconClassName + '"></span>'),
+								var icon = $('<span class="inline-icon ' + iconClassName + ' mediathumb" data-mediafolder="' + mediaLibrary.folder + '"></span>'),
 									verseNumber = verse.find('.verse-num, v-num');
 									
 								if (verseNumber.length > 0) {
@@ -53,7 +137,7 @@ var MediaLibraryPlugin = function(app) {
 								} else {							
 									icon.prependTo(verse);
 								}
-							}
+							//}
 						}	
 					} // libries loop			
 				
@@ -63,59 +147,6 @@ var MediaLibraryPlugin = function(app) {
 			}); // verse loop
 			
 				
-			for (var i=0, il=mediaLibraries.length; i<il; i++) {
-			
-				var mediaLibrary = mediaLibraries[i];
-				
-				(function(mediaLibrary) {
-		
-					// handle clicks
-					content.on('click', '.' + mediaLibrary.iconClassName, function(e) {
-				
-						// clear it out!
-						mediaPopup.body.html('');				
-				
-						var 
-							icon = $(this),
-							verse = icon.closest('.verse, .v'),
-							verseid = verse.attr('data-id'),
-							reference = new bible.Reference(verseid).toString(),
-							mediaForVerse = mediaLibrary.data[verseid];
-							
-						switch (mediaLibrary.type) {
-							
-							case 'image':
-															
-								var imagesHtmlArray = $.map(mediaForVerse, function(item, index) {
-									var url = 'content/media/' + mediaLibrary.folder + '/' + item;
-										return '<li><a href="' + url + '" target="_blank"><img src="' + url + '" /></a></li>';
-									}),					
-									html = imagesHtmlArray.join('');
-		
-								mediaPopup.body.append('<strong>' + reference.toString() + '</strong>');	
-								mediaPopup.body.append($('<ul class="inline-image-library-thumbs">' + html + '</ul>'));
-								
-								break;
-							
-							case 'video':
-								var url = 'content/media/' + mediaLibrary.folder + '/' + mediaForVerse;																
-								//mediaPopup.body.append('<video src="' + url + '" type="video/mp4" controls autoplay style="width:100%; height: auto;"></video>');
-								
-								mediaPopup.body.append('<strong>' + reference.toString() + '</strong>');	
-								mediaPopup.body.append($('<ul class="inline-video-library-thumbs">' + 
-									'<li><a href="' + url + '"><img src="css/images/video.svg" "></a></li>' +
-								  '</ul>'));
-
-								
-								break;																
-								
-						}
-			
-						//mediaPopup.center().show();
-						mediaPopup.position( icon ).show();
-					});
-				})(mediaLibrary);
-			}
 			
 			content.data('has-media', true);
 			

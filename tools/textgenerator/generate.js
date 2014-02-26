771a0b38710903d7e90b80b82f1f94ccbb8ad155
John Dyer <johndyer@gmail.com>
2014-02-26 13:08:38 -0600

    full document open and close

--- tools/textgenerator/generate.js
+++ tools/textgenerator/generate.js
@@ -1,7 +1,9 @@
 // MODULES
 var fs = require('fs'),
 	path = require('path'),
-	bibleData = require('bible_data');
+	bibleData = require('bible_data'),
+	bibleFormatter = require('bible_formatter'),
+	verseIndexer = require('verse_indexer');
 	
 
 //console.log( bibleData.getBookInfoByUnboundCode('40N') );
@@ -12,7 +14,7 @@ var fs = require('fs'),
 var
 	baseOutput = '../../app/content/texts/',
 	baseInput = 'input',
-	createIndex = true;
+	createIndex = false;
 
 console.log('\r\r\r');
 
@@ -27,22 +29,15 @@ function convertFolder(inputPath) {
 			generatorName = info.generator,
 			generator = require('generate_' + generatorName),
 			outputPath = path.join(baseOutput, info['id']),
-			indexOutputPath = path.join(outputPath, 'index');
+			indexOutputPath = path.join(outputPath, 'index'),
+			indexLemmaOutputPath = path.join(outputPath, 'indexlemma');
 			
 		console.log('-----');
 		console.log(info['name'],  outputPath);
 		
 		// DELETE: existing data
-		if (fs.existsSync(outputPath)) {
-			var files = fs.readdirSync(outputPath);
-			
-			// DELETE all files
-			files.forEach(function(data) {
-				var filePath = path.join(outputPath, data);
-				if (fs.statSync(filePath).isFile()) {
-					fs.unlinkSync(filePath);				
-				}
-			});
+		if (fs.existsSync(outputPath)) {		
+			deleteAllFiles(outputPath);
 		} else {
 			fs.mkdirSync(outputPath);
 		}
@@ -50,29 +45,70 @@ function convertFolder(inputPath) {
 		// DELETE: index data
 		if (createIndex) {		
 			if (fs.existsSync(indexOutputPath)) {
-				var files = fs.readdirSync(indexOutputPath);
-				
-				// DELETE all files
-				files.forEach(function(data) {
-					var filePath = path.join(indexOutputPath, data);
-					if (fs.statSync(filePath).isFile()) {
-						fs.unlinkSync(filePath);				
-					}
-				});			
+				deleteAllFiles(indexOutputPath);					
 			} else {
 				fs.mkdirSync(indexOutputPath);
 			}			
+			
+			if (fs.existsSync(indexLemmaOutputPath)) {
+				deleteAllFiles(indexLemmaOutputPath);					
+			} else {
+				fs.mkdirSync(indexLemmaOutputPath);
+			}						
 		}
 			
 		// RUN GENERATOR		
-		generator.generate(inputPath, outputPath, indexOutputPath, info, createIndex);
+		console.time('processText');
+		var data = generator.generate(inputPath, info, createIndex);
+		console.timeEnd('processText');		
+
+		// create chapters
+		console.time('outputFiles');		
+		for (var i=0, il=data.chapterData.length; i<il; i++) {
+		
+			var thisChapter = data.chapterData[i],
+				chapterHtml = bibleFormatter.openDocument(info, thisChapter) +
+								thisChapter.html +
+								bibleFormatter.closeDocument(info, thisChapter),
+																
+				filePath = path.join(outputPath, thisChapter.id + '.html');
+			
+			fs.writeFileSync(filePath, chapterHtml);		
+		}
+		console.timeEnd('outputFiles');
+			
+		// do index
+		if (createIndex && data.indexData) {
+			console.time('createIndex');		
+		
+			verseIndexer.createIndexFiles(indexOutputPath, data.indexData);
+			
+			console.timeEnd('createIndex');	
+			
+			console.time('createLemma');		
+		
+			verseIndexer.createIndexFiles(indexLemmaOutputPath, data.indexLemmaData);
+			
+			console.timeEnd('createLemma');								
+		}
+		
+		// save info
+		var infoPath = path.join(outputPath, 'info.json');
+		fs.writeFileSync(infoPath, JSON.stringify(info));
+				
+		// save about
+		if (data.aboutHtml != '') {
+			var aboutPath = path.join(outputPath, 'about.html');
+			fs.writeFileSync(aboutPath, data.aboutHtml);			
+			
+		}		
 		
 		// OUTPUT HERE?
 		// Right now the individual files do it, but there is a lot of overlap especially with indexs
 		// Maybe return: {chapters: [], indexData: {}, lemmaIndexData: {}} and output here?
 				
 		var endDate = new Date();		
-		console.log('time: ' + MillisecondsToDuration(endDate - startDate));			
+		console.log('-time: ' + MillisecondsToDuration(endDate - startDate));			
 	}	
 }
 
@@ -91,6 +127,21 @@ function convertFolders() {
 	var endDate = new Date();
 	
 	console.log('TOTAL: ' + MillisecondsToDuration(endDate - startDate));
+}
+
+function deleteAllFiles(pathToDelete) {
+	if (fs.existsSync(pathToDelete)) {
+		var files = fs.readdirSync(pathToDelete);
+		
+		// DELETE all files
+		files.forEach(function(data) {
+			var filePath = path.join(pathToDelete, data);
+			if (fs.statSync(filePath).isFile()) {
+				fs.unlinkSync(filePath);				
+			}
+		});
+	}
+	
 }
 
 

2a886aad5caca0df237d699df230ef25b521e803
John Dyer <johndyer@gmail.com>
2014-03-08 12:00:33 -0600

    turn off for touch devices

--- app/js/plugins/lemmamatch.js
+++ app/js/plugins/lemmamatch.js
@@ -8,49 +8,52 @@ var LemmaMatchPlugin = function(app) {
 '}' +
 '</style>').appendTo( $('head') );
 
-	$('.windows-main').on('mouseover','l', function(e) {
-		
-		//console.log('over');
-		
-		var l = $(this);
-		
-		if (l.closest('.search-wrapper').length > 0)
-			return;
-		
-		var s = l.attr('s');
-		
-		if (!s) {
-			return;
-		}
-		
-		var
-			strongs = l.attr('s').replace('G','').replace('H',''),
-			verse = l.closest('.verse, .v'),
-			verseid = verse.attr('data-id'),
-			bookid = verseid.substring(0,2);
-			langPrefix = (bible.OT_BOOKS.indexOf(bookid) > -1) ? 'H' : 'G';
-
-		
-		var strongParts = strongs.split(' ');
-		
-		for (var i=0, il=strongParts.length; i<il; i++) {
-			var strong = strongParts[i];
+	if (!Detection.hasTouch) {
+	
+		$('.windows-main').on('mouseover','l', function(e) {
 			
+			//console.log('over');
 			
-			// ignore Greek article with second word
-			if (strong == '3588' && il > 1) {
-				continue;
+			var l = $(this);
+			
+			if (l.closest('.search-wrapper').length > 0)
+				return;
+			
+			var s = l.attr('s');
+			
+			if (!s) {
+				return;
 			}
 			
-			$('.' + verseid ).find('l[s*="' + strong + '"],l[s*="' + langPrefix + strong + '"]').addClass('lemma-highlight');					
+			var
+				strongs = l.attr('s').replace('G','').replace('H',''),
+				verse = l.closest('.verse, .v'),
+				verseid = verse.attr('data-id'),
+				bookid = verseid.substring(0,2);
+				langPrefix = (bible.OT_BOOKS.indexOf(bookid) > -1) ? 'H' : 'G';
+	
 			
-		}			
-		
-					
-		
-	}).on('mouseout','l', function(e) {
-		$('.lemma-highlight').removeClass('lemma-highlight');
-	});
+			var strongParts = strongs.split(' ');
+			
+			for (var i=0, il=strongParts.length; i<il; i++) {
+				var strong = strongParts[i];
+				
+				
+				// ignore Greek article with second word
+				if (strong == '3588' && il > 1) {
+					continue;
+				}
+				
+				$('.' + verseid ).find('l[s*="' + strong + '"],l[s*="' + langPrefix + strong + '"]').addClass('lemma-highlight');					
+				
+			}			
+			
+						
+			
+		}).on('mouseout','l', function(e) {
+			$('.lemma-highlight').removeClass('lemma-highlight');
+		});
+	}
 };
 
 sofia.plugins.push('LemmaMatchPlugin');

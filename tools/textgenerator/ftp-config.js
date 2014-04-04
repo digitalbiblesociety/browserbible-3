a503b19db7d342ab38f6a21a1643faa79c93c79e
John Dyer <johndyer@gmail.com>
2014-04-04 12:37:20 -0500

    removed

--- tools/textgenerator/ftp-config.js
+++ tools/textgenerator/ftp-config.js
@@ -1,6 +1,6 @@
 {
-	"host": "ftp.theyearinpopculture.com",
-	"user": "pxwifnct_refreshdev",
-	"password": "4$$CL0WN",
-	"directory": "biblewebapp.com/wwwroot/study/content/texts/"
+	"host": "",
+	"user": "",
+	"password": "",
+	"directory": ""
 }

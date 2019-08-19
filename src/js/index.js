33efec5306807d9d4ef978b2f96fed75f3347f0c
John Dyer <johndyer@gmail.com>
2019-08-19 15:00:17 -0500

    New launch code

--- src/js/index.js
+++ src/js/index.js
@@ -1,86 +1,21 @@
+// TODO: Load App Configuration
+    // - features (maps, user notes, etc.)
+    // - logo, colors, etc.
+    // - data available (local, APIs, video, etc.)
 
-$(function() {
-	// hide initial text area
-	$('#startup').hide();
+// Example: enabling two text APIs
+const textLoader = new TextLoader();
+textLoader.addProvider(new LocalTextProvider('content/texts/'));
 
-	// test for local file support
-	if (window.location.protocol === 'file:') {
+// TODO: Load User Settings
+    // - logged in users stors
+    // - localStorage/Cookies
+    // - defaultSettings
 
-		$.ajax({
-			dataType: 'text',
+// Example: app with three windows
+const sofia = new SofiaApp();
+sofia.addWindow(TextController, {textid: 'local:eng_web', sectionid: 'John 2:5'});
+sofia.addWindow(TextController, {textid: 'local:eng_ylt', sectionid: 'Ex 10'});
 
-			url: 'about.html',
-			success: function() {
-				init();
-			},
-			error: function(e) {
-				var modal = new MovableWindow( Math.min(500, $(window).width()) ,250, 'Local Files Error'),
-					errorMessage = '',
-					ua = navigator.userAgent.toLowerCase();
-				//modal.size(500, 200).center();
-
-				if (ua.indexOf('chrome') > -1) {
-					if (ua.indexOf('mac os') > -1) {
-						errorMessage =
-							'<p>Mac, Terminal</p>' +
-							'<code>/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --allow-file-access-from-files</code>';
-					} else
-					if (ua.indexOf('windows') > -1) {
-						errorMessage =
-							'<p>Mac, Terminal</p>' +
-							'<code>/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --allow-file-access-from-files</code>';
-					}
-				} else {
-					errorMessage =
-						'<p>Unknown error loading files (cannot load about.html)' + e + '</p>';
-				}
-
-				modal.body.css({background: '#000', color: '#fff' }).html(
-					'<div style="padding: 20px;">' +
-						errorMessage +
-					'</div>'
-				);
-				modal.show().center();
-			}
-		});
-
-	} else {
-		init();
-	}
-
-	function init() {
-		// load config
-		var params = stringUtility.parseQuerystring(),
-			custom = params["custom"];
-
-		if (typeof custom != 'undefined' && custom != '') {
-			var customizations = sofia.customConfigs[custom];
-
-			if (typeof customizations != 'undefined' && customizations != null) {
-				sofia.config = $.extend(sofia.config, customizations);
-			}
-		}
-
-		// load css
-		if (typeof sofia.config.customCssUrl != 'undefined' && sofia.config.customCssUrl != '') {
-			$('<link href="' + sofia.config.customCssUrl + '" rel="stylesheet" />').appendTo( $('head') );
-		}
-
-		var isiOSApp = (navigator.userAgent.toLowerCase().indexOf('ipad') > -1 || navigator.userAgent.toLowerCase().indexOf('iphone') > -1) && window.location.protocol === 'file:';
-
-		if (window.navigator.standalone === true || isiOSApp) {
-			$('body').addClass('app-mobile-fullscreen');
-		}
-
-		// run inits
-		for (var i=0, il=sofia.initMethods.length; i<il; i++) {
-			sofia.initMethods[i]();
-		}
-
-		// create app
-		sofia.app = new App();
-		sofia.app.init();
-
-		$('.i18n').i18n();
-	}
-});
+// Example: highlight verses across windows
+sofia.addPlugin(VerseHighlighter);

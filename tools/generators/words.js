9e5b683bcac21f14c9f4b52c8dc0d5097f3bfca9
Andreea Olaru <ana.olaru@eosads-all51933-all-dev.mwp-tyrell.e5.c.emag.network>
2023-03-14 00:16:19 +0200

    fix path generation for words

--- tools/generators/words.js
+++ tools/generators/words.js
@@ -7,12 +7,11 @@ function generate(inputPath, info, createIndex, startProgress, updateProgress) {
         strongsText = fs.readFileSync(strongsPath, 'utf8')
         strongsData = JSON.parse(strongsText),
         strongsLemmaKey = {};
-    const contentPath =  path.join('app', 'content');
 
 
     for (var strongsNumber in strongsData) {
-        console.log(contentPath, strongsData[strongsNumber])
-        fs.writeFileSync(contentPath + `/lexicons/entries/${strongsNumber}.json`, JSON.stringify(strongsData[strongsNumber]), (err, result)=>{
+        const contentPath =  path.join('app', 'content', 'lexicons', 'entries', `${strongsNumber}.json`);
+        fs.writeFileSync(contentPath, JSON.stringify(strongsData[strongsNumber]), (err, result)=>{
             if(err) console.log('error', err);
         });
     }
